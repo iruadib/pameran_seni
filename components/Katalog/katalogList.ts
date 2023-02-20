ffd751066d7cc0787cef8b4c25474cda8d948f88
iruadib <khoiruladib422@gmail.com>
2023-02-20 09:05:08 +0700

    change

--- components/Katalog/katalogList.ts
+++ components/Katalog/katalogList.ts
@@ -10,26 +10,16 @@ export interface KatalogProps {
 }
 
 export const mainKatalog: KatalogProps = {
-  name: 'Spooky House (Karya Utama)',
-  img: '/img/Gojou.webp',
-  type: '3D',
+  name: 'Melankolia Art (Karya Utama)',
+  img: '/img/visuals/Melankolia_Art.jpeg',
+  type: '2D',
   pembuat: 'Kelompok Lima',
-  tanggal: '??',
-  panjang: '??',
-  lebar: '??',
-  tinggi: '??'
+  tanggal: '12 Januari 2023',
+  panjang: '60cm',
+  lebar: '40cm'
 }
 
 export const katalogList: KatalogProps[] = [
-  {
-    name: 'Melankolia Art',
-    img: '/img/visuals/Melankolia_Art.jpeg',
-    type: '2D',
-    pembuat: 'Kelompok Lima',
-    tanggal: '12 Januari 2023',
-    panjang: '60cm',
-    lebar: '40cm'
-  },
   {
     name: 'Demon Girl',
     img: '/img/visuals/Demon_girl.jpeg',
@@ -44,13 +34,13 @@ export const katalogList: KatalogProps[] = [
     img: '/img/visuals/An_eye.jpeg',
     type: '2D',
     pembuat: 'Zahira Maulidina',
-    tanggal: '??',
-    panjang: '21cm',
-    lebar: '14.8cm'
+    tanggal: '05 Desember 2022',
+    panjang: '9cm',
+    lebar: '9cm'
   },
   {
-    name: 'Lazy Girl',
-    img: '/img/visuals/Lazy_girl (2).jpeg',
+    name: 'Chilling',
+    img: '/img/visuals/Chill Girl.jpeg',
     type: '2D',
     pembuat: 'Diva Edenia',
     tanggal: '18 Februari 2023',
@@ -72,9 +62,9 @@ export const katalogList: KatalogProps[] = [
     img: '/img/visuals/Starry_night.jpeg',
     type: '2D',
     pembuat: 'Zahira Maulidina',
-    tanggal: '??',
-    panjang: '??',
-    lebar: '??'
+    tanggal: '03 Maret 2021',
+    panjang: '36cm',
+    lebar: '23cm'
   },
   {
     name: 'Alley of Silence',
@@ -84,5 +74,34 @@ export const katalogList: KatalogProps[] = [
     tanggal: '31 Mei 2023',
     panjang: '21cm',
     lebar: '14.8cm'
+  },
+  {
+    name: 'Beautiful Chopstick',
+    img: '/img/visuals/Beautiful_chopstick.jpeg',
+    type: '3D',
+    pembuat: 'Muhammad Maula Khadavi',
+    tanggal: '09 Oktober 2022',
+    panjang: '??',
+    lebar: '??',
+    tinggi: '??'
+  },
+  {
+    name: 'Sand of Life',
+    img: '/img/visuals/Sand_of_life.jpeg',
+    type: '3D',
+    pembuat: 'Muhammad Maula Khadavi',
+    tanggal: '17 Maret 2021',
+    panjang: '??',
+    lebar: '??',
+    tinggi: '??'
+  },
+  {
+    name: 'Candy Girl',
+    img: '/img/visuals/Candy_girl.jpeg',
+    type: '2D',
+    pembuat: 'Zahira Maulidina',
+    tanggal: '22 Januari 2023',
+    panjang: '21cm',
+    lebar: '15cm'
   }
 ]
