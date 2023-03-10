75644ca7553f256b371431904efa57c06f645200
iruadib <khoiruladib422@gmail.com>
2023-03-11 05:45:06 +0700

    change page

---
+++ pages/usul.tsx
@@ -0,0 +1,13 @@
+import { Proposal } from '@/components/Proposal'
+import Head from 'next/head'
+
+export default function UsulPage() {
+  return (
+    <>
+      <Head>
+        <title>Halaman Proposal | The Melankolia</title>
+      </Head>
+      <Proposal />
+    </>
+  )
+}
