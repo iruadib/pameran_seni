40a0ba57632fb4f30b88e10d41340f94213cbdd4
iruadib <khoiruladib422@gmail.com>
2023-03-10 22:31:02 +0700

    fix bug

---
+++ components/GlitchText/index.tsx
@@ -0,0 +1,21 @@
+import styles from './style.module.css'
+
+export const GlitchText = () => {
+  return (
+    <>
+      <div className={styles.cont}>
+        <p className={styles.heading}>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+          <span className={styles.line}>Kelompok Lima</span>
+        </p>
+      </div>
+    </>
+  )
+}
