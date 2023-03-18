49aa4f49d62061dc02c90a01835598050a7f18ff
iruadib <khoiruladib422@gmail.com>
2023-03-18 20:21:22 +0700

    change visuals

--- components/Katalog/katalogList.ts
+++ components/Katalog/katalogList.ts
@@ -30,14 +30,15 @@ export const katalogList: KatalogProps[] = [
     lebar: '29.7cm'
   },
   {
-    name: 'An Eye',
-    img: '/img/visuals/An_eye.jpeg',
+    name: 'Alley of Silence',
+    img: '/img/visuals/Alley_of_silence.jpeg',
     type: '2D',
-    pembuat: 'Zahira Maulidina',
-    tanggal: '05 Desember 2022',
-    panjang: '9cm',
-    lebar: '9cm'
+    pembuat: 'Diva Edenia',
+    tanggal: '31 Mei 2023',
+    panjang: '21cm',
+    lebar: '14.8cm'
   },
+
   {
     name: 'Chilling',
     img: '/img/visuals/Chill Girl.jpeg',
@@ -46,6 +47,54 @@ export const katalogList: KatalogProps[] = [
     tanggal: '18 Februari 2023',
     panjang: '29.7cm',
     lebar: '21cm'
+  },
+  {
+    name: 'Candy Girl',
+    img: '/img/visuals/Candy_girl.jpeg',
+    type: '2D',
+    pembuat: 'Zahira Maulidina',
+    tanggal: '22 Januari 2023',
+    panjang: '21cm',
+    lebar: '15cm'
+  },
+
+  {
+    name: 'Starry Night',
+    img: '/img/visuals/Starry_night.jpeg',
+    type: '2D',
+    pembuat: 'Zahira Maulidina',
+    tanggal: '03 Maret 2021',
+    panjang: '36cm',
+    lebar: '23cm'
+  },
+  {
+    name: 'An Eye',
+    img: '/img/visuals/An_eye.jpeg',
+    type: '2D',
+    pembuat: 'Zahira Maulidina',
+    tanggal: '05 Desember 2022',
+    panjang: '9cm',
+    lebar: '9cm'
+  },
+  // {
+  //   name: 'Beautiful Chopstick',
+  //   img: '/img/visuals/Beautiful_chopstick.jpeg',
+  //   type: '3D',
+  //   pembuat: 'Muhammad Maula Khadavi',
+  //   tanggal: '09 Oktober 2022',
+  //   panjang: '20cm',
+  //   lebar: '12cm',
+  //   tinggi: '20cm'
+  // },
+  {
+    name: 'Beautiful Chopstick',
+    img: '/img/visuals/Beautiful_chopstick_t1.jpeg',
+    type: '3D',
+    pembuat: 'Muhammad Maula Khadavi',
+    tanggal: '09 Oktober 2022',
+    panjang: '20cm',
+    lebar: '12cm',
+    tinggi: '20cm'
   },
   {
     name: 'Smiley Face',
@@ -57,51 +106,36 @@ export const katalogList: KatalogProps[] = [
     lebar: '22cm',
     tinggi: '14cm'
   },
-  {
-    name: 'Starry Night',
-    img: '/img/visuals/Starry_night.jpeg',
-    type: '2D',
-    pembuat: 'Zahira Maulidina',
-    tanggal: '03 Maret 2021',
-    panjang: '36cm',
-    lebar: '23cm'
-  },
-  {
-    name: 'Alley of Silence',
-    img: '/img/visuals/Alley_of_silence.jpeg',
-    type: '2D',
-    pembuat: 'Diva Edenia',
-    tanggal: '31 Mei 2023',
-    panjang: '21cm',
-    lebar: '14.8cm'
-  },
-  {
-    name: 'Beautiful Chopstick',
-    img: '/img/visuals/Beautiful_chopstick.jpeg',
-    type: '3D',
-    pembuat: 'Muhammad Maula Khadavi',
-    tanggal: '09 Oktober 2022',
-    panjang: '20cm',
-    lebar: '12cm',
-    tinggi: '20cm'
-  },
+  // {
+  //   name: 'Sand of Life',
+  //   img: '/img/visuals/Sand_of_Lifes.jpeg',
+  //   type: '3D',
+  //   pembuat: 'Muhammad Maula Khadavi',
+  //   tanggal: '17 Maret 2021',
+  //   panjang: '8cm',
+  //   lebar: '6cm',
+  //   tinggi: '30cm'
+  // },
+  // *********************
   {
     name: 'Sand of Life',
-    img: '/img/visuals/Sand_of_Lifes.jpeg',
+    img: '/img/visuals/Sand_of_Lifes_t1.jpeg',
     type: '3D',
     pembuat: 'Muhammad Maula Khadavi',
     tanggal: '17 Maret 2021',
     panjang: '8cm',
     lebar: '6cm',
     tinggi: '30cm'
-  },
-  {
-    name: 'Candy Girl',
-    img: '/img/visuals/Candy_girl.jpeg',
-    type: '2D',
-    pembuat: 'Zahira Maulidina',
-    tanggal: '22 Januari 2023',
-    panjang: '21cm',
-    lebar: '15cm'
   }
+  // {
+  //   name: 'Sand of Life',
+  //   img: '/img/visuals/Sand_of_Lifes_t2.jpeg',
+  //   type: '3D',
+  //   pembuat: 'Muhammad Maula Khadavi',
+  //   tanggal: '17 Maret 2021',
+  //   panjang: '8cm',
+  //   lebar: '6cm',
+  //   tinggi: '30cm'
+  // },
+  // *********************
 ]
